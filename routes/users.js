91f7b6dfac0c2a005283667cb821278ea2dfc497
Mahmud <menon1673@gmail.com>
2018-06-20 16:46:04 +0600

    Facebook OAUTH

--- routes/users.js
+++ routes/users.js
@@ -21,6 +21,7 @@ router.get('/', authenticate.verifyUser, authenticate.verifyAdmin, (request, res
       .catch((err) => next(err));
 });
  
+ 
 
 
 
@@ -76,6 +77,16 @@ router.get('/logout', (req, res) => {
   }
 });
 
+router.get('/facebook/token', passport.authenticate('facebook-token'), (req, res) => {
+  if (req.user) {
+    var token = authenticate.getToken({_id: req.user._id});
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({success: true, token: token, status: 'You are successfully logged in!'});
+  }
+});
+
 
 
 module.exports = router;
+  
